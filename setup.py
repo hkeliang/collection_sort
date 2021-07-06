9c3e514100006ec3d47ad4996a63fb0b3241e836
root <root@DESKTOP-B140O06.localdomain>
2021-07-06 21:03:21 +0800

    add sort

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[],
     entry_points={
         'collection_sort': [
-            'quick_sort=quick_sort:quick_sort'
+            'quicksort=quicksort:quick_sort'
         ]
     }
 )
